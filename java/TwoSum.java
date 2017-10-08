dae2c96bb6512a497f8f3dfcd0936a302865d7e4
邢小辉 <1559237979@qq.com>
2017-10-08 17:53:57 +0800

    Update TwoSum.java

--- java/TwoSum.java
+++ java/TwoSum.java
@@ -4,7 +4,7 @@ import java.util.Map;
 /**
  * Created by codingBoy on 17/2/3.
  */
-public class two_sum {
+public class TwoSum {
     public static void main(String[] args)
     {
         int[] numbers=new int[]{2,7,11,15};
